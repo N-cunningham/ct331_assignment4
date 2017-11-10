59a142168e51431f37962b07e341a66945ae7125
Niall Cunningham <nccunningham@hotmail.com>
2017-11-10 19:16:32 +0000

    Add files via upload

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,23 @@
 %Definitions for isElementInList(El, List)
 
-%Definitions for reverseList(List, ReversedList)
 
-%insertElementIntoListEnd(El, List, NewList)
+
+isElementInList(El, [El | List]).
+
+isElementInList(El, [Head|List]):- isElementInList(El, List).
+
 
 %Definitions for mergeLists(List1, List2, Merged)
+mergeLists([], List2, List2).
+mergeLists([Item | T1], List2, [Item | Merged]) :-
+      mergeLists(T1, List2, Merged).
+
+
+
+%Definitions for reverseList(List, ReversedList)
+
+
+
+
+
+%insertElementIntoListEnd(El, List, NewList)
